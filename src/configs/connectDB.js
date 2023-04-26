b0fb13e3d453cc5e4127dc5ebda2cac39cc04ab9
Khanh Truong <tckhanh.dev@gmail.com>
2023-04-26 17:03:17 +0700

    aa

---
+++ src/configs/connectDB.js
@@ -0,0 +1,16 @@
+// get the client
+import mysql from 'mysql2/promise';
+
+// create the connection to database
+
+console.log("Create connection pool...");
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'nodejsbasic'
+});
+
+
+
+export default pool;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+import initWebRoute from './route/web';
+import connection from './configs/connectDB';
+
+require('dotenv').config();
 
 // const express = require('express');
 
 const app = express();
-const port = 1011;
 
+const port = process.env.PORT || 2803;
+console.log('>> Link Localhot:', port)
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//setup view engine
 configViewEngine(app);
-
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
-app.get('/about', (req, res) => {
-    res.send(`I'am Khanh`)
-})
+//init web route
+initWebRoute(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
